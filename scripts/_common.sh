0c2058c3566496e92793f6046505885ecaf52cda
Anton Bagliy <taccessviolation@gmail.com>
2022-01-04 01:10:40 +0300

    FEAT: backup, remove and restore

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-# pkg_dependencies="deb1 deb2 php$YNH_DEFAULT_PHP_VERSION-deb1 php$YNH_DEFAULT_PHP_VERSION-deb2"
+pkg_dependencies="ffmpeg"
 
 #=================================================
 # PERSONAL HELPERS
@@ -18,3 +18,7 @@
 #=================================================
 # FUTURE OFFICIAL HELPERS
 #=================================================
+
+youtubedl_exists () {
+  type "youtube-dl" >/dev/null 2>/dev/null
+}
